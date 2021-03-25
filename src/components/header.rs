1a7f5ba3defce191bb1729dc996880b35a20a987
tim.mulqueen <tim.mulqueen@gmail.com>
2021-03-25 23:14:07 +0100

    fail to get props active

--- src/components/header.rs
+++ src/components/header.rs
@@ -7,6 +7,21 @@ pub struct Header {
     current_location: String,
 }
 
+fn get_location() -> String {
+    let window = web_sys::window().expect("Unable to find window");
+    let location = window
+        .location()
+        .pathname()
+        .expect("Unable to get pathname from location");
+    log::info!("found location {:?}", location);
+    return location;
+}
+
+fn is_active(matcher: &str) -> bool {
+    let current_location = get_location();
+    return current_location.contains(matcher);
+}
+
 pub enum Msg {}
 
 impl Component for Header {
@@ -33,20 +48,33 @@ impl Component for Header {
         false
     }
 
+    fn rendered(&mut self, _first_render: bool) {
+        self.current_location = get_location();
+    }
+
     fn view(&self) -> Html {
-        let active_classes = "mt-4";
-        // self.current_location.contains("recipe") &&
+        let active_classes_recipe = if is_active("recipes") {
+            "text-blue-300"
+        } else {
+            " "
+        };
+        let active_classes_ingredients = if is_active("ingredients") {
+            "text-blue-300"
+        } else {
+            " "
+        };
+        // self.current_location.contains("recipe")
         log::info!("in vuiew: {:?}", self.current_location);
         html! {
           <header class="container m-auto p-4 text-left flex flex-row justify-between items-center">
             <AppAnchor classes="mr-2" route=AppRoutes::Home >
                 <h1 class="text-2xl text-secondary font-bold mb-1">{"Whats Cooking"}</h1>
             </AppAnchor>
-            <nav>
-                <AppAnchor classes=(format!("mr-2 {}", active_classes)) route=AppRoutes::RecipesPage >
+            <nav class="space-x-2">
+                <AppAnchor classes=active_classes_recipe route=AppRoutes::RecipesPage >
                     {"Recipes"}
                 </AppAnchor>
-                <AppAnchor route=AppRoutes::IngredientsPage>
+                <AppAnchor classes=active_classes_ingredients route=AppRoutes::IngredientsPage>
                     {"Ingredient"}
                 </AppAnchor>
             </nav>
